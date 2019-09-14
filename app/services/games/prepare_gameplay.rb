4b0903ebca343c1463cd6993dbefacecc9238ef6
Alex223124 <sologoubalex@gmail.com>
2019-09-14 16:00:27 +0200

    service for gameplaye creation added

---
+++ app/services/games/prepare_gameplay.rb
@@ -0,0 +1,44 @@
+class Services::Games::PrepareGameplay
+
+  BASIC_AMOUNT_OF_FRAMES_FOR_SINGLE_PLAYER = 10
+  BASIC_AMOUT_OF_THROWS_FOR_SINGLE_FRAME = 2
+
+  def initialize(game)
+    @game = game
+    @steps_counter = 0
+  end
+
+  def prepare
+    build_gameplay
+  end
+
+  private
+
+  def build_gameplay
+    BASIC_AMOUNT_OF_FRAMES_FOR_SINGLE_PLAYER.times do
+      @game.players.each do |player|
+        # Фрейм 1 Бросок 1 Степ 1 @counter+=1 Бросок 2 Степ 2 @counter+=2
+        frame = build_frame(player)
+        build_throws(frame)
+        build_steps(frame.throws)
+      end
+    end
+  end
+
+  def build_frame(player)
+    player.frames.create!(type: "basic_ten_frames")
+  end
+
+  def build_throws(frame)
+    BASIC_AMOUT_OF_THROWS_FOR_SINGLE_FRAME.times do
+      frame.throws.create!
+    end
+  end
+
+  def build_steps(throws)
+    throws.each do |throw|
+      throw.create_step!(game:@game, position: @steps_counter+=1)
+    end
+  end
+
+end
