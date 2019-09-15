7f156e4801e314ac4382a4c25ca5ebe653454154
Alex223124 <sologoubalex@gmail.com>
2019-09-15 02:11:05 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :games, only: [:new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/steps/:game_id', to: 'steps#current'
+  resources :steps, only: [:update]
+
 end
