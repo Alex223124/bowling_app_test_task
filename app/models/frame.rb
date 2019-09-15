b47b19fb106f70ecc8fa948423477f64dd4dafd8
Alex223124 <sologoubalex@gmail.com>
2019-09-15 03:42:31 +0200

    constanta for bonus frame added, boolean method for checking if this frame is last basic frame added

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,8 +1,13 @@
 class Frame < ApplicationRecord
 
+  BONUS_FRAME_NUMBER = 11
   self.inheritance_column = nil
 
   belongs_to :player
   has_many :throws
+
+  def is_last_basic_frame?
+    self.number == 10
+  end
 
 end
