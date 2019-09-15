e9fe887ac62fa2bd63f0c50b89fdf0acc8570c03
Alex223124 <sologoubalex@gmail.com>
2019-09-15 02:16:05 +0200

    #current_step method updated

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,10 @@
 class Game < ApplicationRecord
 
   has_many :players
+  has_many :steps, -> { order(position: :asc) }
+
+  def current_step
+    steps.where(is_completed: false, is_skipped: false).first
+  end
 
 end
