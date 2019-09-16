c7fdd0e4bad1b83b04d6bdd7e18299d3a1b65ec4
Alex223124 <sologoubalex@gmail.com>
2019-09-16 19:50:18 +0200

    helper methods for views added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def point_value(frame)
+    value = frame.point.value
+    if frame.throws.where(number_of_knocked_down_pins: nil).count == 2
+      "Waiting for both throws to be completed"
+    elsif value
+      value
+    elsif value == nil
+      "Status: #{frame.point.status} | Status reason: #{frame.point.status_reason}"
+    end
+  end
+
+  def throw_result(throw)
+    if throw.number_of_knocked_down_pins == nil && !throw.step.is_skipped
+      "Throw is pending"
+    elsif throw.step.is_skipped
+      "Skipped, reason why skipped: #{throw.step.reason_why_skipped}"
+    else
+      throw.number_of_knocked_down_pins
+    end
+  end
+
 end
