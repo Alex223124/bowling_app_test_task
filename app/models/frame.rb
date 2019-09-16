0dc696bbe49e8620ae7b39f33da644743373ea33
Alex223124 <sologoubalex@gmail.com>
2019-09-16 19:55:28 +0200

    new boolean methods added, added methods 'next' and 'previous'

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,13 +1,40 @@
 class Frame < ApplicationRecord
 
+  FIRST_FRAME_NUMBER_FOR_PLAYER = 1
   BONUS_FRAME_NUMBER = 11
   self.inheritance_column = nil
 
   belongs_to :player
   has_many :throws
+  has_one :point
 
   def is_last_basic_frame?
     self.number == 10
+  end
+
+  def is_bonus_frame?
+    self.number == BONUS_FRAME_NUMBER
+  end
+
+  def first_in_game?
+    self.previous == nil
+  end
+
+  def both_throws_completed?
+    throws.where.not(number_of_knocked_down_pins: nil).count == 2
+  end
+
+  def sum_of_two_throws
+    throws.first.number_of_knocked_down_pins +
+    throws.second.number_of_knocked_down_pins
+  end
+
+  def next
+    Frame.where("player_id = ? AND number > ?", player.id, number).first
+  end
+
+  def previous
+    Frame.where("player_id = ? AND number < ?", player.id, number).last
   end
 
 end
