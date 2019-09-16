fa749185ed1414b34a0b3a6b86d92a83c117bbd1
Alex223124 <sologoubalex@gmail.com>
2019-09-16 19:45:06 +0200

    added bonus throws generation and calculation of throw result, all under transaction in #update action

--- app/services/steps/update.rb
+++ app/services/steps/update.rb
@@ -8,18 +8,36 @@ class Services::Steps::Update
 
   def call
     ActiveRecord::Base.transaction do
-      @throw.mark_throw_result(@params[:number_of_knocked_down_pins])
-
-      if @throw.is_strike?
-        @throw.mark_as_strike
-        @step.try(:next).try(:mark_as_skipped, "strike_by_first_throw")
-      elsif @throw.is_spare?
-        @throw.mark_as_spare
-      else
-        # regular case
-      end
-      @step.mark_as_completed
+      mark_throw
+      add_bonus_throws
+      calculate_throw_result
     end
+  end
+
+  private
+
+  def mark_throw
+    @throw.mark_throw_result(@params[:number_of_knocked_down_pins])
+
+    if @throw.is_strike?
+      @throw.mark_as_strike
+      @step.frame.throws.where.not(id: @throw.id).first.step.try(:mark_as_skipped, "strike_by_first_throw")
+    elsif @throw.is_spare?
+      @throw.mark_as_spare
+    else
+      # regular case
+    end
+    @step.mark_as_completed
+  end
+
+  def add_bonus_throws
+    if @step.frame.is_last_basic_frame?
+      Services::Games::Gameplay::PrepareBonus.new(@step.frame).call
+    end
+  end
+
+  def calculate_throw_result
+    Services::Points::Calculate.new(@step).call
   end
 
 end
