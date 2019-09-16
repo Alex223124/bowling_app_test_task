d0dd8dfd064d703cb9eba9bc4d91f41361383670
Alex223124 <sologoubalex@gmail.com>
2019-09-16 19:48:53 +0200

    new routes for new actions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :games, only: [:new, :create]
+  resources :games, only: [:new, :create, :index, :show]
 
   get '/steps/:game_id', to: 'steps#current'
   resources :steps, only: [:update]
